7e7992a61f318045d88fc564bfbbd00de7a6814c
10cheon00 <10cheon00@gmail.com>
2026-04-04 17:29:39 +0900

    fix: 헤더파일에 정의된 함수원형에 static을 붙였던 오류 수정
    헤더파일에 정의된 함수 원형에 static을 붙인다면, 헤더파일을 참조하는
     모든 c파일은 해당 c파일 내에서만 그 함수의 실체를 참조하려 한다.
    현재는 main.c에 해당 함수들이 구현되지 않아서 undefiend symbol
     에러가 발생하고 있었다(static이 붙어 있으니 main.c에서만 참조하려고
     하기 때문임).
    따라서 헤더파일에 정의된 함수 원형애서 static을 제거함.
    
    인터페이스를 제공하기만 하고 구현체는 직접 정의하도록 의도하려면,
     헤더파일에 정의된 함수 원형에 static을 붙이면 되겠다.

--- Core/Inc/st7789.h
+++ Core/Inc/st7789.h
@@ -54,7 +54,7 @@ typedef struct {
  *
  */
 
-static st7789_status_t
+st7789_status_t
 st7789_init_handle(st7789_handle_t *handle, SPI_HandleTypeDef *hspi,
                    GPIO_TypeDef *GPIO_Port_CS, GPIO_TypeDef *GPIO_Port_SDA,
                    GPIO_TypeDef *GPIO_Port_SCL, GPIO_TypeDef *GPIO_Port_DC,
@@ -62,8 +62,8 @@ st7789_init_handle(st7789_handle_t *handle, SPI_HandleTypeDef *hspi,
                    uint16_t GPIO_Pin_SDA, uint16_t GPIO_Pin_SCL,
                    uint16_t GPIO_Pin_DC, uint16_t GPIO_Pin_RST);
 
-static st7789_status_t st7789_init_display(st7789_handle_t *handle);
+st7789_status_t st7789_init_display(st7789_handle_t *handle);
 
-static st7789_status_t st7789_print_sample_display(st7789_handle_t *handle);
+st7789_status_t st7789_print_sample_display(st7789_handle_t *handle);
 
 #endif
--- Core/Src/st7789.c
+++ Core/Src/st7789.c
@@ -71,7 +71,7 @@ static st7789_status_t st7789_send_command(st7789_handle_t *handle,
     return status;
 }
 
-static st7789_status_t
+st7789_status_t
 st7789_init_handle(st7789_handle_t *handle, SPI_HandleTypeDef *hspi,
                    GPIO_TypeDef *GPIO_Port_CS, GPIO_TypeDef *GPIO_Port_SDA,
                    GPIO_TypeDef *GPIO_Port_SCL, GPIO_TypeDef *GPIO_Port_DC,
@@ -92,7 +92,7 @@ st7789_init_handle(st7789_handle_t *handle, SPI_HandleTypeDef *hspi,
     return STATUS_OK;
 }
 
-static st7789_status_t st7789_init_display(st7789_handle_t *handle) {
+st7789_status_t st7789_init_display(st7789_handle_t *handle) {
     st7789_status_t status = STATUS_OK;
     uint16_t command = SWRESET;
     uint16_t parameter = 0;
@@ -162,6 +162,6 @@ static st7789_status_t st7789_init_display(st7789_handle_t *handle) {
     return STATUS_OK;
 }
 
-static st7789_status_t st7789_print_sample_display(st7789_handle_t *handle) {
+st7789_status_t st7789_print_sample_display(st7789_handle_t *handle) {
     return STATUS_OK;
 }
